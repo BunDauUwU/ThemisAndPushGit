eee5d793c80257e5ae0e8d8f742f4348375ca8c0
Bún <162699708+BunDauUwU@users.noreply.github.com>
2025-10-26 11:54:20 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+#define               el  "\n"
 #define               fi  first
 #define               se  second
 #define               pb  push_back
@@ -11,23 +12,26 @@ using namespace std;
 #define        BIT(x, i)  ((x) >> (i) & 1)
 #define        CNTBIT(x)  __builtin_popcountll(x)
 #define           ALL(v)  (v).begin(), (v).end()
-#define             FAST  cin.tie(nullptr) -> ios_base::sync_with_stdio(false);
+#define            FAST   cin.tie(nullptr) -> ios_base::sync_with_stdio(false);
+#define         OI(FILE)  if(FILE != ""){freopen(FILE".inp","r",stdin);freopen(FILE".out","w",stdout);}\
+                          else{freopen("uwu.inp","r",stdin);freopen("uwu.out","w",stdout);}
 
 typedef        long long  ll;
 typedef      long double  ld;
 typedef      pair<ll,ll>  pii;
 
-const ll      N  =  1e5 + 5;
-const ll      M  =  1e5 + 5;
+const ll      N  =  2e5 + 5;
+const ll      M  =  2e5 + 5;
 const ll    mod  =  1e9 + 7;
 const ll    INF  =  1e18;
 bool checksub[]  =  {0, 1, 1, 1, 1};
+
+struct FAST_IO{FAST_IO(){cin.tie(nullptr) -> ios_base::sync_with_stdio(false);};}; static FAST_IO fast;
 
 template <class X, class Y> void add(X &a, Y b){
     a += b;
     if(a < 0) a += mod;
     if(a >= mod) a -= mod;
-    a%=mod;
 }
 template <class X, class Y> bool ckmin(X &a, Y b){
     return (a > b ? a = b, 1:0);
@@ -36,40 +40,12 @@ template <class X, class Y> bool ckmax(X &a, Y b){
     return (a < b ? a = b, 1:0);
 }
 
-template <int MOD>struct modnum {
-  int val;
-
-  modnum(ll v = 0) { val = v % MOD; if (val < 0) val += MOD;}
-  modnum(ll a, ll b) : val(0){ *this += a; *this /= b;}
-
-  modnum& operator+=(modnum& b) {val += b.val; if (val >= MOD) val -= MOD; return *this;}
-  modnum& operator-=(modnum& b) {val -= b.val; if (val < 0) val += MOD;return *this;}
-  modnum& operator*=(modnum& b) {val = (ll)val * b.val % MOD;return *this;}
-
-  friend modnum pw(modnum a, ll e) {
-    modnum res = 1;
-    for(e; e; e >>= 1){
-        if(e & 1) e *= (a);
-        a *= a;
-    }
-    return res;
-  }
-  friend modnum inverse(modnum a) { return pw(a, MOD - 2); }
-
-  modnum& operator/=(modnum& b) { return *this *= inverse(b); }
-
-  friend modnum operator+(modnum a, modnum  b) { return a += b; }
-  friend modnum operator-(modnum a, modnum  b) { return a -= b; }
-  friend modnum operator-(modnum  a) { return 0 - a; }
-  friend modnum operator*(modnum a, modnum  b) { return a *= b; }
-  friend modnum operator/(modnum a, modnum  b) { return a /= b; }
-
-  friend ostream& operator<<(ostream& out, modnum& a) {return out << a.val;}
-  friend bool operator==(modnum& a, modnum& b) {return a.val == b.val;}
-  friend bool operator!=(modnum& a, modnum& b) {return a.val != b.val;}
+template<class X> struct compress{
+    vector<X> c;
+    compress(vector<X> v){c = v; sort(ALL(c)); c.erase(unique(ALL(c)), c.end()); }
+    int operator() (int a){ return lower_bound(ALL(c), a) - c.begin(); }
 };
 
-using num = modnum<mod>;
 /////////////////////////
 ///.........../\__/\..///
 ///..........(=o_o= ).///
@@ -81,19 +57,14 @@ void solve(){
 }
 
 Elaina {
-    FAST
-//    #define FILE ""
-    #ifdef FILE
-        freopen(FILE".inp","r",stdin);freopen(FILE".out","w",stdout);
-    #else
-        freopen("uwu.inp","r",stdin);freopen("uwu.out","w",stdout);
-    #endif
+    OI("");
 
     ll tabitabi = 1;
-    //cin >> tabitabi;
+//    cin >> tabitabi;
     while(tabitabi-- ) {
         solve();
     }
 
+    cerr << "Time: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
     return 0;
 }
