8c7a50d01261e0d515e0d7f8534439927628b4cf
Bún <162699708+BunDauUwU@users.noreply.github.com>
2025-10-28 08:47:43 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,8 +26,6 @@ const ll    mod  =  1e9 + 7;
 const ll    INF  =  1e18;
 bool checksub[]  =  {0, 1, 1, 1, 1};
 
-struct FAST_IO{FAST_IO(){cin.tie(nullptr) -> ios_base::sync_with_stdio(false);};}; static FAST_IO fast;
-
 template <class X, class Y> void add(X &a, Y b){
     a += b;
     if(a < 0) a += mod;
@@ -43,28 +41,18 @@ template <class X, class Y> bool ckmax(X &a, Y b){
 template<class X> struct compress{
     vector<X> c;
     compress(vector<X> v){c = v; sort(ALL(c)); c.erase(unique(ALL(c)), c.end()); }
-    int operator() (int a){ return lower_bound(ALL(c), a) - c.begin(); }
+    int operator() (X& a){ return lower_bound(ALL(c), a) - c.begin(); }
 };
 
-/////////////////////////
-///.........../\__/\..///
-///..........(=o_o= ).///
-///.....[Blackcat!]<\.///
-/////////////////////////
+struct IO{IO(){cin.tie(nullptr) -> ios_base::sync_with_stdio(false);}}; static IO Setup;
+struct TIME{~TIME(){cerr << "Time: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";}}; TIME time_setup;
 
-void solve(){
-
-}
+///////////////////////
+//.........../\__/\..//
+//..........(=o_o= ).//
+//.....[Blackcat!]<\.//
+///////////////////////
 
 Elaina {
-    OI("");
 
-    ll tabitabi = 1;
-//    cin >> tabitabi;
-    while(tabitabi-- ) {
-        solve();
-    }
-
-    cerr << "Time: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
-    return 0;
 }
